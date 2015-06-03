9b0a69e2f5442a984d62189ba8748c7caf388d92
Jonathan <jonathan.corallo92@gmail.com>
2015-06-02 21:43:21 -0300

    Agregado consulta por nombre de paciente

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,20 @@
 class WelcomeController < ApplicationController
 
+  @pacientes
   # GET /welcome
   def index
+      @neo = Neography::Rest.new(ENV['NEO4J'])
+      busqueda_paciente()
+  end
 
+  def busqueda_paciente
+
+    unless params[:text_box_nombre_paciente].nil?
+      query_busqueda_pacientes = "MATCH (n:PERSONA) WHERE n.nombre =~ '(?i).*"+ params[:text_box_nombre_paciente] +".*' RETURN n LIMIT 5"
+    else
+      query_busqueda_pacientes = "MATCH (n:PERSONA) RETURN n LIMIT 5"
+    end
+    @pacientes = @neo.execute_query(query_busqueda_pacientes)
   end
 
 end
