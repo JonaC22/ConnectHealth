d9154a165fec834c4127d286172d5910896d101e
Jonathan <jonathan.corallo92@gmail.com>
2015-06-12 10:47:52 -0300

    extract persons and relations

--- app/controllers/pedigree_controller.rb
+++ app/controllers/pedigree_controller.rb
@@ -2,21 +2,28 @@ class PedigreeController < ApplicationController
 
   # GET /api/pedigree
   def index
-    neo = Neography::Rest.new(ENV['NEO4J'])
-    query_busqueda_pacientes = "match (n{nombre:'#{params[:name]}'})-[rel]-(n2:PERSONA) return n, n2,rel"
-    pacientes = neo.execute_query(query_busqueda_pacientes)
+    @neo = Neography::Rest.new(ENV['NEO4J'])
+    query_busqueda_pacientes = "match (n:PERSONA{nombre:'#{params[:name]}'})-[*]-(n2:PERSONA) return n, n2"
+    pacientes = @neo.execute_query(query_busqueda_pacientes)
     personas = Hash.new
     relaciones = Hash.new
-    # personas << Person.new(4,params[:name])
+
+    #Se extraen personas y relaciones
     pacientes["data"].each_with_index do |persona,index|
-      p0 = Person.new(persona[0]["metadata"]["id"],persona[0]["data"]["nombre"])
-      p1 =Person.new(persona[1]['metadata']["id"],persona[1]["data"]["nombre"])
-      personas[p0.id] =  p0
-      personas[p1.id] =  p1
-      relaciones[persona[2]['metadata']['id']] = Relation.new(p0.id,p1.id,persona[2]['metadata']['type'])
+      persona.each do |per|
+        p = Person.new( per["metadata"]["id"], per['data']['nombre'])
+        personas[p.id] = p
+      end
     end
-    #p1 = Person.new(params[:name],Person.new('Luis'),Person.new('Vilma'))
+
+    #Se extraen relaciones
+    personas.each_with_index do |persona, index|
+      nodo = Neography::Node.load(persona.id,@neo)
+      relaciones[index] = nodo.incoming
+    end
+
     result = Hash.new
+
     result['personas'] =  personas
     result['relations'] = relaciones
     render json:result
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,11 +3,8 @@ class Person
   attr_accessor :children,:id
 
   def initialize(id,name,father=nil,mother=nil,children=[])
-    @id=id
-    @name= name
-    @father= father
-    @mother=mother
-    @children=children
+    attr_accessor :id, :name, :father, :mother, :children
+
     # unless father==nil
     #   father.children.append(self)
     # end
