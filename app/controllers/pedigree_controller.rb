b2523ab6ff840e2128e7978e58f5bfdea774744e
fede1608 <fedecasares1993@yahoo.com.ar>
2015-06-14 17:21:18 -0300

    Post de nodos y relaciones

--- app/controllers/pedigree_controller.rb
+++ app/controllers/pedigree_controller.rb
@@ -1,8 +1,11 @@
 class PedigreeController < BaseController
-
+  before_filter :initialize
+  skip_before_filter :verify_authenticity_token
+  def initialize
+    @neo = Neography::Rest.new(ENV['NEO4J'])
+  end
   # GET /api/pedigree
   def index
-    @neo = Neography::Rest.new(ENV['NEO4J'])
     query_busqueda_pacientes = "match (n:PERSONA{nombre:'#{params[:name]}'})-[*]-(n2:PERSONA) return n, n2"
     patients = @neo.execute_query(query_busqueda_pacientes)
     persons = Hash.new
@@ -37,6 +40,7 @@ class PedigreeController < BaseController
   end
 
   before_filter only: :create do
+    @json = JSON.parse(request.body.read)
     unless @json.has_key?('personas') && @json.has_key?('relations')
       render nothing: true, status: :bad_request
     end
@@ -44,9 +48,15 @@ class PedigreeController < BaseController
 
 #POST /api/pedigree
   def create
-    @json = JSON.parse(request.body.read)
-    unless @json.has_key?('project') && @json['project'].responds_to?(:[]) && @json['project']['name']
-      render nothing: true, status: :bad_request
-    end
+    persons = Hash.new
+    @json['personas'].each { |persona|
+      node = @neo.create_node("edad" => persona['edad'], "nombre" => persona['nombre'],'sexo' => persona['sexo'], 'posX' => persona['posX'], 'posY' => persona['posY'])
+      @neo.set_label(node,"PERSONA")
+      persons[persona['id']] = node
+    }
+    @json['relations'].each { |rel|
+      @neo.create_relationship(rel['name'], persons[rel['from']], persons[rel['to']])
+    }
+    render json:@json
   end
 end
