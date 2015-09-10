4c99081e6d7921196c066cc5a521de272e9e631e
UrielMys <uri_mysler@hotmail.com>
2015-09-10 08:59:16 -0300

    follow up on merge

--- app/controllers/pedigrees_controller.rb
+++ app/controllers/pedigrees_controller.rb
@@ -57,33 +57,6 @@ class PedigreesController < BaseController
     resultado = Resultado.new('Pedigree ingresado exitosamente', 200)
     render json: resultado
   end
-  # Se pasa por parametro el nodo del paciente
-  # Devuelve un par clave-valor, donde la clave es el id del pariente y el valor un array de id de enfermedades padecidas.
-  def get_first_deg_relatives(node)
-    ret = []
-    ret.push(*node.both(:MADRE))
-    ret.push(*node.both(:PADRE))
-
-    relatives = {}
-    rel_ids = ret.map(&:neo_id)
-    rel_ids.each do |relative_id|
-      n = Neography::Node.load(relative_id, @neo)
-      diseases = []
-      diseases.push(*n.outgoing(:PADECE))
-      diseases = diseases.map(&:nombre)
-      relatives.store(relative_id, diseases)
-    end
-
-    relatives
-  end
-
-  def validate_relations(json, persona, tags)
-    tags.each do |tag|
-      count = json['relations'].count { |rel| rel['from'] == persona['id'] && rel['name'] == tag }
-      return Resultado.new("Relacion duplicada: #{tag}", 500) if count > 1
-    end
-    Resultado.new('OK', 200)
-  end
 
   #GET /api/pedigree/gailModelCalculate
   def calculate_gail_model
@@ -109,103 +82,128 @@ class PedigreesController < BaseController
     # 2=[2, 31]
 
     #  RiskIndex           [1 Abs, 2 Avg]
-    #, CurrentAge       //[t1] edad actual ( tiene que ser mayor a 35)
-    #, ProjectionAge      //[t2]
-    #, AgeIndicator     //[i0]
-    #, NumberOfBiopsy     //[i2] cant de biopsias de mamas 1, 2(2 o mas)
-    #, MenarcheAge        //[i1] edad de primera menstruacion
-    #, FirstLiveBirthAge   //[i3] 2 ()never) , 0 (<20),1(20-24), 2(25-29),3(>=30)
-    #, EverHadBiopsy      //[iever] 0 no, 1 yes, 99 unknown
-    #, HyperPlasia        //[ihyp] 0 no, 1 yes, 99 unknown
-    #, FirstDegRelatives   //[i4] 0, 1 or 2(2 or more)
-    #, RHyperPlasia     //[rhyp] 0 no, 1 yes, 99 unknown
-    #, Race         //[race] 1-white 3-hispanic 6-unknown
+    # , CurrentAge       //[t1] edad actual ( tiene que ser mayor a 35)
+    # , ProjectionAge      //[t2]
+    # , AgeIndicator     //[i0]
+    # , NumberOfBiopsy     //[i2] cant de biopsias de mamas 1, 2(2 o mas)
+    # , MenarcheAge        //[i1] edad de primera menstruacion
+    # , FirstLiveBirthAge   //[i3] 2 ()never) , 0 (<20),1(20-24), 2(25-29),3(>=30)
+    # , EverHadBiopsy      //[iever] 0 no, 1 yes, 99 unknown
+    # , HyperPlasia        //[ihyp] 0 no, 1 yes, 99 unknown
+    # , FirstDegRelatives   //[i4] 0, 1 or 2(2 or more)
+    # , RHyperPlasia     //[rhyp] 0 no, 1 yes, 99 unknown
+    # , Race         //[race] 1-white 3-hispanic 6-unknown
 
     calculator = RiskCalculator.new
     patient = Person.create_from_neo params[:id]
-    #agregar validacion de que paciente no debe tener la enfermedad de la que se esta evaluando el riesgo
+    current_age = patient.age
+
+    if patient.gender == 'M'
+      error = {status: 'ERROR', message: 'Algoritmo no aplicable a pacientes de sexo masculino'}
+      return render json: error
+    end
+
+    if patient.diseases.include? 'Cancer de Mama'
+      error = {status: 'ERROR', message: 'El paciente que ya padece la enfermedad'}
+      return render json: error
+    end
+    #TODO agregar validacion para pacientes fallecidos algoritmo no aplicable
+    if current_age > 90
+      error = {status: 'ERROR', message: 'Algoritmo no aplicable a pacientes mayores a 90 años'}
+      return render json: error
+    end
+
+    if current_age < 35
+      error = {status: 'ERROR', message: 'Algoritmo no aplicable a pacientes menores a 35 años'}
+      return render json: error
+    end
+
     fdr = patient.get_first_deg_relatives
+
     affected_relatives = fdr.count {
-        |value|
+      |key, value|
       unless value.nil?
         value.include? 'Cancer de Mama'
       end
     }
-    current_age = patient.age
+
     projection_age=current_age+5
-    menarche_age = BcptConvert.MenarcheAge(params[:menarchAge].to_i)
-    #TODO obtener edad de primer hijo
-    first_live_birth_age=BcptConvert.FirstLiveBirthAge(params[:first_birth_age].to_i)
+
+    menarche_age = BcptConvert.MenarcheAge(params[:menarcheAge].to_i)
+    first_live_birth_age=BcptConvert.FirstLiveBirthAge(patient.get_first_live_birth_age.to_i)
     age_indicator=BcptConvert.CurrentAgeIndicator(current_age)
-    ever_had_biopsy_bool = false
-    ever_had_biopsy = ever_had_biopsy_bool ? 1 : 0
-    number_of_biopsy = ever_had_biopsy_bool ? params[:numberBiopsy].to_i : 0
-    race = 3 #Hispanic
-    first_deg_relatives = BcptConvert.FirstDegRelatives(affected_relatives,race)
-    ihyp=BcptConvert.hyperplasia(0,ever_had_biopsy_bool)
+    ever_had_biopsy = params[:numberBiopsy].to_i > 0
+    number_of_biopsy = ever_had_biopsy ? BcptConvert.number_of_biopsy(params[:numberBiopsy].to_i,true) : 0
+    race = 1 # White or Unknown
+    first_deg_relatives = BcptConvert.FirstDegRelatives(affected_relatives, race)
+    ihyp=BcptConvert.hyperplasia(0, ever_had_biopsy)
     rhyp=BcptConvert.r_hyperplasia(ihyp)
+
+
     # calculate_absolute_risk(current_age, projection_age, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, irace)
+    logger.info "Gail Params: current_age, projection_age, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race "
+    logger.info [current_age, projection_age, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race].to_s
     abs_risk = calculator.calculate_absolute_risk(current_age, projection_age, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race)
     avg_risk = calculator.calculate_average_risk(current_age, projection_age, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race)
     abs_risk90 = calculator.calculate_absolute_risk(current_age, 90, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race)
     avg_risk90 = calculator.calculate_average_risk(current_age, 90, age_indicator, number_of_biopsy, menarche_age, first_live_birth_age, first_deg_relatives, ever_had_biopsy, ihyp, rhyp, race)
-    # abs_risk = RiskCalculator.new.calculate_absolute_risk(38,43,0,0,2,BcptConvert.FirstLiveBirthAge(0),2,0,0,1.0,1)
+    # abs_risk = RiskCalculator.new.calculate_absolute_risk(66,71,1,0,2,BcptConvert.FirstLiveBirthAge(17),2,0,99,1.0,1)
     # avg_risk = RiskCalculator.new.calculate_average_risk(38,43,0,0,2,BcptConvert.FirstLiveBirthAge(0),2,0,0,1.0,1)
-    # abs_risk90 = RiskCalculator.new.calculate_absolute_risk(38,90,0,0,2,BcptConvert.FirstLiveBirthAge(0),2,0,0,1.0,1)
+    # abs_risk90 = RiskCalculator.new.calculate_absolute_risk(66,90,1,0,2,BcptConvert.FirstLiveBirthAge(17),2,0,99,1.0,1)
     # avg_risk90 = RiskCalculator.new.calculate_average_risk(38,90,0,0,2,BcptConvert.FirstLiveBirthAge(0),2,0,0,1.0,1)
     result = {'absoluteRiskIn5Years' => abs_risk, 'averageRiskIn5Years' => avg_risk,'absoluteRiskAt90yo' => abs_risk90, 'averageRiskAt90yo' => avg_risk90}
     render json: result
   end
 
-  #GET /api/pedigree/query
+  # GET /api/pedigree/query
   def query
     id_current_patient = params[:id]
     type = params[:type] || nil
 
-    # query genérica que devuelve todos los familiares que padecen una enfermedad
+    # query generica que devuelve todos los familiares que padecen una enfermedad
     match = " match (n)-[r:PADECE]->(e)
-              where ((n)-[:PADRE|MADRE*]-(n2) and id(n2) = #{id_current_patient}) or
-              id(n) = #{id_current_patient} "
+    where ((n)-[:PADRE|MADRE*]-(n2) and id(n2) = #{id_current_patient}) or
+    id(n) = #{id_current_patient} "
     case type
     when 'integer'
-      execute_and_render match << 'return count(r) as cantidad_casos'
+      execute_and_render match << " return count(r) as cantidad_casos "
     when 'float'
-      execute_and_render match << 'return avg(r.edad_diagnostico) as promedio_edad_diagnostico'
+      execute_and_render match << " return avg(r.edad_diagnostico) as promedio_edad_diagnostico "
     when 'table'
-      execute_and_render match << 'return r.edad_diagnostico as edad_diagnostico'
-    when 'pedigree' # Obtiene el pedigree recortado
-      match = " match ca = (n:PERSONA)-[:PADRE|MADRE*]-(n2), (n2:PERSONA)-[:PADECE]->(e)
-                where id(n) = #{id_current_patient}
-                with nodes(ca) as nodos
-                unwind nodos as nodo
-                return nodo "
-      patients = @neo.execute_query match
+      execute_and_render match << " return r.edad_diagnostico as edad_diagnostico "
+      when 'pedigree' #Obtiene el pedigree recortado
+        match = " match ca = (n:PERSONA)-[:PADRE|MADRE*]-(n2), (n2:PERSONA)-[:PADECE]->(e)
+        where id(n) = #{id_current_patient}
+        with nodes(ca) as nodos
+        unwind nodos as nodo
+        return nodo "
+        patients = @neo.execute_query match
 
-      visualize patients, id_current_patient
-    else
-      result = { status: 'ERROR', results: 'Formato de respuesta no especificado' }
+        visualize patients, id_current_patient
+      else
+        result = {"status" => "ERROR", "results" => "Formato de respuesta no especificado"}
+        render json: result
+      end
+    end
+
+    def execute_and_render match
+      result = @neo.execute_query match
       render json: result
     end
-  end
 
-  def execute_and_render(match)
-    result = @neo.execute_query match
-    render json: result
-  end
+    private
 
-  private
-
-  def pedigree_find_params
-    { id: params[:id],
-      current_patient: params[:current_patient]
-    }
-  end
-
-  def validate_relations(json, persona, tags)
-    tags.each do |tag|
-      count = json['relations'].count { |rel| rel['from'] == persona['id'] && rel['name'] == tag }
-      return Resultado.new("Relacion duplicada: #{tag}", 500) if count > 1
+    def pedigree_find_params
+      { id: params[:id],
+        current_patient: params[:current_patient]
+      }
     end
-    Resultado.new('OK', 200)
+
+    def validate_relations(json, persona, tags)
+      tags.each do |tag|
+        count = json['relations'].count { |rel| rel['from'] == persona['id'] && rel['name'] == tag }
+        return Resultado.new("Relacion duplicada: #{tag}", 500) if count > 1
+      end
+      Resultado.new('OK', 200)
+    end
   end
-end
