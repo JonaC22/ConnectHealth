961f39a9938ba084997aae11a5da65d1d85f9724
UrielMys <uri_mysler@hotmail.com>
2015-10-03 13:07:11 -0300

    remove signed from cookies display name

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:user_display_name] = user.display_name
+    cookies.permanent[:user_display_name] = user.display_name
     cookies.permanent[:remember_token] = user.remember_token
   end
 
